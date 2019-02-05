337d140c6e0916ef4740fa499c6884c19cb8ebb1
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-02-05 08:47:01 +0100

    ArrayList

--- App2/App.java
+++ App2/App.java
@@ -2,7 +2,8 @@ package App2;
 //@author fsancheztemprano
 public class App {
     public static void main(String[] args) {
-
+        int filas = 5;
+        int[][] piramide = new int[filas][filas];
         
         
     }    
